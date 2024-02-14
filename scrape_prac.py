997d78783b099847b9ff3bcb3c77d68eafb288b2
Chido Nguyen <chidonguyen@hotmail.com>
2024-02-13 17:38:53 -0800

    practice and robot.txt parsing html

---
+++ scrape_prac.py
@@ -0,0 +1,17 @@
+from urllib.request import urlopen
+
+target_site = "https://www.espn.com" #fill in or user prompt
+
+#check for robots.txt and see if what we're doing is allowed
+
+target_site +=  "robots.txt" if target_site[-1] == "/" else "/robots.txt"
+
+#open url/get html txt to prep parse
+
+page = urlopen(target_site)
+html_bytes = page.read()
+html = html_bytes.decode('utf-8')
+
+print(html)
+
+

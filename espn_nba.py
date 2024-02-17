75d4e5d648facd4d44d1501bd18e0e9b1378c99e
Chido Nguyen <chidonguyen@hotmail.com>
2024-02-17 10:32:51 -0800

    can obtain /nba/ subpath href, working on tests

--- espn_nba.py
+++ espn_nba.py
@@ -1,8 +1,10 @@
 #extracting matchups for NBA games happening today
 
 from bs4 import BeautifulSoup
+from bs4.diagnose import diagnose
 from urllib.request import urlopen
-
+import urllib
+import re
 
 decoder_type = "utf-8"
 
@@ -12,13 +14,23 @@ def parse_NBA(url = "https://www.espn.com"):
 	
 	#nav espn for the NBA link
 	try:
-		nba_page = urlopen(url)
+		#nba_page = urlopen(url)
+		with urlopen(url) as nbapage:
+			soup = BeautifulSoup(nbapage,'lxml',from_encoding = 'utf-8')
 	except urllib.error.HTTPError as e:
 		print(e)
-	except urllib.error.URLerror as u:
+	except urllib.error.URLError as u:
 		print(u)
-
-	raw_html = nba_page.read().decode(decoder_type)
-	
-
-parse_NBA()
+	#diagnose(soup)
+	#print(soup.get_text())
+	#find_all picks up all tags with 'a' / hyperlink we  narrow it down for NBA to exist in the tagline somewhere
+	#we can treat .Tag as dictionaries i is a tag object we can get the links via .get('href')
+	tmp = soup.find_all("a", string="NBA")
+	for i in soup.find_all('a'):
+		if i.get('href') == '/nba/':
+			return url + i.get('href')
+def testNBA():
+	print(parse_NBA())
+		
+testNBA()		
+#parse_NBA()
---
+++ espn_nba_test.py
@@ -0,0 +1,9 @@
+import espn_nba.py
+import unittest
+
+'''
+assert : [Equal, NotEqual, True, False , Is , IsNot, IsNone, IsNotNone, In, NotIn, IsInstance, NotInstance]
+	 
+'''
+class Test_ESPN_NBA(unittest.Testcase):
+	def test_

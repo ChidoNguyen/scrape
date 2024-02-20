901fc763a4142cce764c65e527ef3cae7fcf1ac1
Chido Nguyen <chidonguyen@hotmail.com>
2024-02-19 18:12:57 -0800

    migrating my dev location

--- espn_nba.py
+++ espn_nba.py
@@ -31,3 +31,32 @@ def parse_NBA(url = "https://www.espn.com"):
 			return url + i.get('href')
 	return ""
 
+#refactor a bit to make finding specific href more flexible
+
+#process get soup of our main starting site
+
+def starting_soup(site_URL):
+	try:
+		with urlopen(site_URL) as espn_nba:
+			soup = BeautifulSoup(espn_nba, 'lxml')
+	except urllib.error.HTTPError as e:
+		print(e)
+	except urllib.error.URLerror as u:
+		print(u)
+
+	return soup
+
+def final_endpt(soup,target):
+	#expand for crawl or user input later (?)
+	target = "https//www.espn.com/nba/scoreboard"
+	t = BeautifulSoup(urlopen("https://www.espn.com/nba"),'lxml')
+	oops = t.find_all('a')
+	for items in oops:
+		print(items.get('href'))
+def main():
+	url = "https://www.google.com"
+	soup = starting_soup(url)
+	final_endpt(soup,"")
+	#we can add user input to pick which link they want to follow but for now aiming to get to NBA->Schedule or Scoreboard
+
+main()
